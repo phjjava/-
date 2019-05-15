d2d81ecb6544751d84c52ea0bbd8e54faedc9c54
phj <phj@panhj>
2019-05-15 17:14:01 +0800

    修改家谱管理-章节管理接口

--- src/main/java/com/jp/controller/IntroduceController.java
+++ src/main/java/com/jp/controller/IntroduceController.java
@@ -10,7 +10,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jp.common.ConstantUtils;
+import com.jp.common.JsonResponse;
+import com.jp.common.MsgConstants;
 import com.jp.common.PageModel;
+import com.jp.common.Result;
 import com.jp.entity.Introduce;
 import com.jp.service.IntroduceService;
 
@@ -34,59 +39,65 @@ public class IntroduceController {
 		}
 		return "genealogy/introduce";
 	}
-	 @RequestMapping(value="/list",method = RequestMethod.POST)
-	    public String list(PageModel<Introduce> pageModel,Introduce introduce, ModelMap model){
-	 	   try {
-	 		  itservice.pageQuery(pageModel,introduce);
-	 		if(pageModel.getList()!=null){
-	 			if(pageModel.getPageSize()==0){
-	 				if(pageModel.getPageNo()!=null&&!"1".equals(pageModel.getPageNo())){
-	 					pageModel.setPageNo(pageModel.getPageNo() - 1);
-	 					itservice.pageQuery(pageModel,introduce);
-	 				}
-	 			}
-	 		}
-	 		model.put("pageModel", pageModel);
-	 		model.put("introduce", introduce);
+	@RequestMapping(value="/list",method = RequestMethod.POST)
+	@ResponseBody
+	public JsonResponse list(PageModel<Introduce> pageModel,Introduce introduce){
+		Result result = null;
+		JsonResponse res = null;
+		try {
+			itservice.pageQuery(pageModel,introduce);
+			if(pageModel.getList()!=null){
+				if(pageModel.getPageSize()==0){
+					if(pageModel.getPageNo()!=null&&!"1".equals(pageModel.getPageNo())){
+						pageModel.setPageNo(pageModel.getPageNo() - 1);
+						itservice.pageQuery(pageModel,introduce);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			log_.error("[JPSYSTEM]", e);
+			result = new Result(MsgConstants.SYS_ERROR);
+			res = new JsonResponse(result);
+			return res;
+		}
+		result = new Result(MsgConstants.RESUL_SUCCESS);
+		res = new JsonResponse(result);
+		res.setData(pageModel);
+		return res;
+	}
+    @RequestMapping(value = "/batchDelete", method = RequestMethod.POST)
+    @ResponseBody
+    public String batchDelete(String introduceids){
+    	String result=null;
+    	try {
+	 		String introduceid = introduceids.substring(0, introduceids.length());
+	 		String introduceArray [] = introduceid.split(",");
+	 		itservice.batchDelete(introduceArray);
+	 		result="1";
 	 	} catch (Exception e) {
+	 		result = "0";
 	 		e.printStackTrace();
 	 		log_.error("[JPSYSTEM]", e);
 	 	}
-	 	   return "genealogy/introduceList";
-	 	   
-	    }
-	 
-	 @ResponseBody
-	    @RequestMapping(value = "/batchDelete", method = RequestMethod.POST)
-	    public String batchDelete(String introduceids){
-	    	String result=null;
-	    	try {
-		 		String introduceid = introduceids.substring(0, introduceids.length());
-		 		String introduceArray [] = introduceid.split(",");
-		 		itservice.batchDelete(introduceArray);
-		 		result="1";
-		 	} catch (Exception e) {
-		 		result = "0";
-		 		e.printStackTrace();
-		 		log_.error("[JPSYSTEM]", e);
-		 	}
-		    	return result;
+	    	return result;
+	}
+    
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@ResponseBody
+	public String saveIntroduce(Introduce introduce,HttpServletRequest request)  {
+		String result = null;
+		try{
+			if (request.getCharacterEncoding() == null) {
+				request.setCharacterEncoding("UTF-8");
+				}
+			itservice.saveIntroduce(introduce);
+		    result = "1";
+		}catch(Exception e){
+			result = "0";
+			e.printStackTrace();
+			log_.error("[JPSYSTEM]", e);
 		}
-	 @ResponseBody
-		@RequestMapping(value = "/save", method = RequestMethod.POST)
-		public String saveIntroduce(Introduce introduce,HttpServletRequest request)  {
-			String result = null;
-			try{
-				if (request.getCharacterEncoding() == null) {
-					request.setCharacterEncoding("UTF-8");
-					}
-				itservice.saveIntroduce(introduce);
-			    result = "1";
-			}catch(Exception e){
-				result = "0";
-				e.printStackTrace();
-				log_.error("[JPSYSTEM]", e);
-			}
-			return result;
-		}
+		return result;
+	}
 }
