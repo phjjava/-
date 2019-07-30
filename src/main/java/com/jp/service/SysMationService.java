f959bf13aed2bc7c2c04a3531dc4ce01ddde5c54
tom_glb <goodMorning_glb@atguigu.com>
2019-07-30 17:27:13 +0800

    资讯类型

--- src/main/java/com/jp/service/SysMationService.java
+++ src/main/java/com/jp/service/SysMationService.java
@@ -4,17 +4,58 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.jp.entity.MationType;
 import com.jp.entity.SysMation;
 
 public interface SysMationService {
-
+	/**
+	 * 咨询列表
+	 * @param goType
+	 * @return
+	 */
 	List<SysMation> selectByGoType(String goType);
-
+	/**
+	 * 修改
+	 * @param mation
+	 * @return
+	 */
 	Integer update(SysMation mation);
-
+	/**
+	 * 新增
+	 * @param mation
+	 * @return
+	 */
 	Integer insert(SysMation mation);
-
+	/**
+	 * 物理批量删除
+	 * @param mationids
+	 * @return
+	 * @throws Exception
+	 */
 	int batchDeleteAll(@Param("array") String mationids[]) throws Exception;
+	/**
+	 * 类型下拉框
+	 * @return
+	 */
+	List<MationType> selecttypelist();
+	
+	/**
+	 * 资讯类型修改
+	 * @param mationtype
+	 * @return
+	 */
+	Integer updatetype(MationType mationtype);
+	/**
+	 * 资讯类型新增
+	 * @param mationtype
+	 * @return
+	 */
+	Integer inserttype(MationType mationtype);
+	/**
+	 * 资讯类型批删
+	 * @param mationtypeArray
+	 */
+	int mationtypeDeleteAll(@Param("array")String[] mationtypeArray)throws Exception;
 
 	
 
