f463f74ed8fcd19faef9d9bbf904227ce761acad
phj <phj@panhj>
2019-08-21 09:11:34 +0800

    修改验证

--- src/main/java/com/jp/dao/HomeStateMapper.java
+++ src/main/java/com/jp/dao/HomeStateMapper.java
@@ -3,7 +3,6 @@ package com.jp.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.jp.entity.HomeState;
@@ -20,9 +19,6 @@ public interface HomeStateMapper extends BaseMapper<HomeState> {
 
 	@Select("SELECT * FROM `jp_home_state` WHERE userid = #{userid}")
 	HomeState selectByUserid(@Param("userid") String userid);
-
-	@Update("UPDATE jp_home_state SET #{param1} = #{param2} WHERE userid = #{param3}")
-	int updateByUserid(String code, int status, String userid);
 
 	int updateByUseridSelective(HomeState homeState);
 
