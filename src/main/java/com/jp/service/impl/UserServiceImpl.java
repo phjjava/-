167f74d26be98681cd4dd516ca27b42181dcd64c
tom_glb <goodMorning_glb@atguigu.com>
2019-09-05 11:26:44 +0800

    婚姻状态更改

--- src/main/java/com/jp/service/impl/UserServiceImpl.java
+++ src/main/java/com/jp/service/impl/UserServiceImpl.java
@@ -1890,7 +1890,7 @@ public class UserServiceImpl implements UserService {
 				userMates.setMatename(user1.getUsername());
 				userMates.setSex(user.getSex());
 				userMates.setDeleteflag(0);
-				userMates.setIsMarry(1);
+				userMates.setIsMarry(0);
 				userMates.setPid(user1.getPid());
 				userMates.setPname(user1.getPname());
 				userMates.setPinyinfirst(PinyinUtil.getPinYinFirstChar(user.getMatename()));
@@ -1906,7 +1906,8 @@ public class UserServiceImpl implements UserService {
 					userDao.insertSelective(userMates);
 					userInfo.setUserid(userid);
 					userInfoDao.insertSelective(userInfo);
-					//修改配偶信息
+					//修改配偶信息及婚配状态
+					user.setIsMarry(0);
 					userDao.updateByPrimaryKeySelective(user);
 					result = new Result(MsgConstants.RESUL_SUCCESS);
 				}
